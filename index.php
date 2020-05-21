ae8486f715574cd07984c12b58425c9f7e272810
tyno43 <tnyo43@gmail.com>
2020-05-21 18:42:20 +0900

    move todolist into index

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+
+    require_once('config.php');
+    require_once('database.php');
+
+    session_start();
+
+    try {
+        $pdo = new PDO(DSN, DB_USER, DB_PASS);
+        $stmt = $pdo->prepare(findToDoByUserId());
+        $stmt->execute([$_SESSION['user_id']]);
+        $items = $stmt;
+    } catch (Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+        return false;
+    }
+
+?>
+
+<?php
+    session_start();
+    if (empty($_SESSION['user_id'])) {
+        header('Location: signIn.php');
+        exit;
+    }
+
     $pageTitle = 'top';
     require('head.php');
 ?>
@@ -8,7 +33,22 @@
         require('header.php');
     ?>
 
-    <p>TODO list<p>
+    <table>
+        <tr>
+            <th>title</th>
+            <th>details</th>
+            <th>deadline</th>
+        </tr>
+        <?php
+            foreach ($items as $item) {
+                echo "<tr>
+                        <td>{$item['title']}</td>
+                        <td>{$item['details']}</td>
+                        <td>{$item['deadline']}</td>
+                    </tr>";
+            }
+        ?>
+    </table>
 
     <input type="button" onclick="location.href='./registerItem.php'" value="add item">
 </body>
