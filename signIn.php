d6550d8e5a954859a4689ef74c7539a7f325965e
tyno43 <tnyo43@gmail.com>
2020-05-21 18:20:02 +0900

    fix, name signin and signup

--- signIn.php
+++ signIn.php
@@ -1,35 +1,38 @@
 <?php
 
-    require_once('config.php');
-    require_once('database.php');
-        
-    try {
-        $pdo = new PDO(DSN, DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->exec(createUserTable());
-    } catch (Exception $e) {
-        echo $e->getMessage() . PHP_EOL;
-    }
+    session_start();
 
-    $name = $_POST['name'];
-    if (preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $_POST['password'])) {
-        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    } else {
-        echo "Make sure it's at least 8 characters including a number AND a lowercase letter. ";
-        return false;
-    }
-
-    $stmt = $pdo->prepare(findUserByUsername());
-    $stmt->execute([$name]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!isset($row['name'])) {
-        $stmt = $pdo->prepare(insertUser());
-        $stmt->execute([$name, $password]);
-        echo 'registered!';
-    } else {
-        echo 'name, '. $name . ', is already used.';
-        return false;
+    if (isset($_SESSION['name'])) {
+        echo 'Welcome, ' .  htmlspecialchars($_SESSION['name'], ENT_QUOTES, 'utf-8') . "!<br>";
+        echo "<a href='/logout.php'>logout</a>";
+        exit;
     }
 
 ?>
+
+<!DOCTYPE html>
+<htm>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+</head>
+<body>
+    <h1>sign in</h1>
+    <form  action="login.php" method="post">
+        <label for="name">name</label>
+        <input type="name" name="name">
+        <label for="password">password</label>
+        <input type="password" name="password">
+        <button type="submit">sign in</button>
+    </form>
+    <h1>sign up</h1>
+        <form action="signUp.php" method="post">
+        <label for="name">name</label>
+        <input type="name" name="name">
+        <label for="password">password</label>
+        <input type="password" name="password">
+        <button type="submit">sign up</button>
+        <p>Make sure it's at least 8 characters including a number AND a lowercase letter.</p>
+    </form>
+</body>
+</html>
